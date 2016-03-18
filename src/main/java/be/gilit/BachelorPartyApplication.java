e643ee56aecba824023bdcf699e7ae86c9299a9c
timmygilissen <timmy.gilissen@gmail.com>
2016-03-18 21:55:39 +0100

    first commit

---
+++ src/main/java/be/gilit/BachelorPartyApplication.java
@@ -0,0 +1,12 @@
+package be.gilit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorPartyApplication.class, args);
+	}
+}
